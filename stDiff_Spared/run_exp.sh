76003d179ca21c4c6986f90940616e0e1518f07f
dlmanrique <dl.manrique@uniandes.edu.co>
2024-12-26 16:30:27 -0500

    Minor changes

--- stDiff_Spared/run_exp.sh
+++ stDiff_Spared/run_exp.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=5 python main_2D.py --dataset villacampa_lung_organoid --prediction_layer c_t_deltas --batch_size 512
-CUDA_VISIBLE_DEVICES=5 python main_2D.py --dataset villacampa_mouse_brain --prediction_layer c_t_deltas --batch_size 1024
+CUDA_VISIBLE_DEVICES=3 python main_2D.py --dataset mirzazadeh_mouse_brain
+CUDA_VISIBLE_DEVICES=3 python main_2D.py --dataset erickson_human_prostate_cancer_p1
