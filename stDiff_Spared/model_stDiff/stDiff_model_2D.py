Fix normalization bug - Investigando tema de best_val vs test_val

stDiff_Spared/model_stDiff/stDiff_model_2D.py
@@ -191,8 +191,7 @@ class DiTblock(nn.Module):
         approx_gelu = lambda: nn.GELU()
         
         mlp_hidden_dim = int(feature_dim * mlp_ratio)
-        self.mlp = Mlp(in_features=feature_dim, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
-        
+        self.mlp = Mlp(in_features=feature_dim, hidden_features=mlp_hidden_dim, act_layer=approx_gelu)
         
         self.adaLN_modulation = nn.Sequential(
             nn.SiLU(),
@@ -435,6 +434,7 @@ class DiT_stDiff(nn.Module):
             t = self.time_emb(t) # t.shape (batch) -> (batch, hidden_size)
             y = self.cond_layer(y[0]) # shape (batch, UNI features dim) -> (batch, hidden_size)
             cond = t + y
+            #cond = t
 
             for blk in self.blks:
                 x = blk(x, cond)

stDiff_Spared/model_stDiff/stDiff_train.py
@@ -33,6 +33,7 @@ def normal_train_stDiff(model,
                  avg_tensor,
                  wandb_logger,
                  args,
+                 st_data_train,
                  st_data_val,
                  adata_valid,
                  lr: float = 1e-4,
@@ -63,10 +64,10 @@ def normal_train_stDiff(model,
     )
 
     #Define Loss function
-    criterion = nn.MSELoss()
+    criterion = nn.MSELoss(reduction=args.reduction_type)
     model.to(device)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=0)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=0.1)
     #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=20, verbose=True, threshold=1e-4, cooldown=10, min_lr=5e-8)
     
@@ -131,28 +132,48 @@ def normal_train_stDiff(model,
         
         # compare MSE metrics and save best model
         if epoch % (num_epoch//10) == 0 and epoch != 0:
-            
-            metrics_dict, imputation_data = inference_function(dataloader=valid_dataloader,
-                                        data=st_data_val, 
-                                        model=model,
-                                        max_norm = max_norm[1],
-                                        min_norm = min_norm[1],
-                                        avg_tensor = avg_tensor,
-                                        diffusion_step=args.diffusion_steps_train,
-                                        device=device,
-                                        args=args
-                                        )
-            #breakpoint()
-            adata_valid.layers["diff_pred"] = imputation_data
-            log_pred_image_extreme_completion(adata_valid, args, epoch)
+            model.eval()
+            with torch.no_grad():
 
-            if metrics_dict["MSE"] < min_mse:
-                min_mse = metrics_dict["MSE"]
-                best_mse = metrics_dict["MSE"]
-                best_pcc = metrics_dict["PCC-Gene"]
+                metrics_dict_val, imputation_data_val = inference_function(dataloader=valid_dataloader,
+                                            data=st_data_val, 
+                                            model=model,
+                                            max_norm = max_norm[1],
+                                            min_norm = min_norm[1],
+                                            avg_tensor = avg_tensor,
+                                            diffusion_step=args.diffusion_steps_train,
+                                            device=device,
+                                            args=args
+                                            )
+
+                adata_valid.layers["diff_pred"] = imputation_data_val
+                #log_pred_image_extreme_completion(adata_valid, args, epoch)
+                
+                metrics_dict_train, imputation_data_train = inference_function(dataloader=valid_dataloader,
+                                            data=st_data_val, 
+                                            model=model,
+                                            max_norm = max_norm[1],
+                                            min_norm = min_norm[1],
+                                            avg_tensor = avg_tensor,
+                                            diffusion_step=args.diffusion_steps_train,
+                                            device=device,
+                                            args=args
+                                            )
+            
+                
+                
+
+            if metrics_dict_val["MSE"] < min_mse:
+                min_mse = metrics_dict_val["MSE"]
+                best_mse = metrics_dict_val["MSE"]
+                best_pcc = metrics_dict_val["PCC-Gene"]
                 torch.save(model.state_dict(), os.path.join("Experiments", exp_name, save_path))
             #save_metrics_to_csv(args.metrics_path, args.dataset, "valid", metrics_dict)
-            wandb_logger.log({"MSE": metrics_dict["MSE"], "PCC": metrics_dict["PCC-Gene"]})
+            wandb_logger.log({"MSE_val": metrics_dict_val["MSE"], "PCC_val": metrics_dict_val["PCC-Gene"]})
+            #wandb_logger.log({"MSE_train": metrics_dict_train["MSE"], "PCC_train": metrics_dict_train["PCC-Gene"]})
+
+            model.train()
+
     # Save the best MSE and best PCC on the validation set
-    wandb_logger.log({"best_MSE":best_mse, "best_PCC": best_pcc})
+    wandb_logger.log({"best_MSE_val":best_mse, "best_PCC_val": best_pcc})
 

stDiff_Spared/train_image_to_genes.py
@@ -41,11 +41,11 @@ def main():
     wandb.login()
     if args.debbug_wandb:
         exp_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        wandb.init(project="debbugs", entity="spared_v2", name=exp_name + '_debbug')
+        wandb.init(project="debbugs_v2", entity="spared_v2", name=exp_name + '_debbug')
 
     else:
         exp_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        wandb.init(project="stDiff_Modelo_2D", entity="spared_v2", name=exp_name )
+        wandb.init(project="Image_to_Genes", entity="spared_v2", name=exp_name )
     #wandb.init(project="Diffusion_Models_NN", entity="sepal_v2", name=exp_name)
     wandb.config = {"lr": args.lr, "dataset": args.dataset}
     wandb.log({"lr": args.lr, 
@@ -65,7 +65,8 @@ def main():
                'scheduler_fixed': True,
                "diffusion_steps_train": args.diffusion_steps_train, 
                "diffusion_steps_test": args.diffusion_steps_test, 
-               'noise_scheduler': args.noise_scheduler})
+               'noise_scheduler': args.noise_scheduler,
+               'reduction': args.reduction_type})
     
     ### Parameters
     # Define the training parameters
@@ -162,6 +163,7 @@ def main():
                             avg_tensor = avg_tensor,
                             wandb_logger=wandb,
                             args=args,
+                            st_data_train=norm_st_data_train,
                             st_data_val=norm_st_data_valid,
                             adata_valid = val_adata,
                             lr=lr,
@@ -174,25 +176,51 @@ def main():
 
     if "test" in splits:
         model.load_state_dict(torch.load(os.path.join("Experiments", exp_name, save_path_prefix)))
+        model.eval()
+        
+        metrics_dict_train, imputation_data_train = inference_function(dataloader=train_dataloader,
+                                    data=norm_st_data_train, 
+                                    model=model,
+                                    max_norm = max_train,
+                                    min_norm = min_train,
+                                    avg_tensor = avg_tensor,
+                                    diffusion_step=args.diffusion_steps_test,
+                                    device=device,
+                                    args=args
+                                    )
+        
+        metrics_dict_val, imputation_data_val = inference_function(dataloader=val_dataloader,
+                                        data=norm_st_data_valid, 
+                                        model=model,
+                                        max_norm = max_valid,
+                                        min_norm = min_valid,
+                                        avg_tensor = avg_tensor,
+                                        diffusion_step=args.diffusion_steps_test,
+                                        device=device,
+                                        args=args
+                                        )
+
+        #log_pred_image_extreme_completion(adata_valid, args, epoch)
+        
+    
         test_metrics, imputation_data = inference_function(dataloader=test_dataloader,
                                         data= norm_st_data_test, 
                                         model=model,
                                         max_norm = max_test,
                                         min_norm = min_test,
                                         avg_tensor = avg_tensor,
-                                        diffusion_step=args.diffusion_steps_train,
+                                        diffusion_step=args.diffusion_steps_test,
                                         device=device,
                                         args=args
                                         )
-
         adata_test = adata[adata.obs["split"]=="test"]
         adata_test.layers["diff_pred"] = imputation_data
         #torch.save(imputation_data, os.path.join('Predictions', f'predictions_{args.dataset}.pt'))
         #log_pred_image_extreme_completion(adata_test, args, -1)
         #save_metrics_to_csv(args.metrics_path, args.dataset, "test", test_metrics)
+        wandb.log({"test_MSE_train": metrics_dict_train["MSE"], "test_PCC_train": metrics_dict_train["PCC-Gene"]})
+        wandb.log({"test_MSE_val": metrics_dict_val["MSE"], "test_PCC_val": metrics_dict_val["PCC-Gene"]})
         wandb.log({"test_MSE": test_metrics["MSE"], "test_PCC": test_metrics["PCC-Gene"]})
-        #print(test_metrics)
-
 
 
 

stDiff_Spared/utils.py
@@ -25,6 +25,12 @@ str2floatlist = lambda x: [float(i) for i in x.split(',')]
 str2h_list = lambda x: [str2intlist(i) for i in x.split('//')[1:]]
 
 
+#Seed
+seed = 1202
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
 
 def get_main_parser():
     parser = argparse.ArgumentParser(description='Code for Diffusion Imputation Model')
@@ -45,13 +51,14 @@ def get_main_parser():
     parser.add_argument('--lr',type=float,default=0.0001,help='lr to use')
     parser.add_argument('--num_epoch', type=int, default=3000, help='Number of training epochs')
     parser.add_argument('--diffusion_steps_train', type=int, default=1500, help='Number of diffusion steps')
-    parser.add_argument('--diffusion_steps_test', type=int, default=1500, help='Number of diffusion steps')
-    parser.add_argument('--batch_size', type=int, default=128, help='The batch size to train model')
+    parser.add_argument('--diffusion_steps_test', type=int, default=50, help='Number of diffusion steps')
+    parser.add_argument('--batch_size', type=int, default=256, help='The batch size to train model')
     parser.add_argument('--optim_metric',                   type=str,           default='MSE',                      help='Metric that should be optimized during training.', choices=['PCC-Gene', 'MSE', 'MAE', 'Global'])
     parser.add_argument('--optimizer',                      type=str,           default='Adam',                     help='Optimizer to use in training. Options available at: https://pytorch.org/docs/stable/optim.html It will just modify main optimizers and not sota (they have fixed optimizers).')
     parser.add_argument('--momentum',                       type=float,         default=0.9,                        help='Momentum to use in the optimizer if it receives this parameter. If not, it is not used. It will just modify main optimizers and not sota (they have fixed optimizers).')
     parser.add_argument('--step_size',                       type=float,         default=600,                         help='Step size to use in learning rate scheduler')
     parser.add_argument("--scheduler",                        type=str2bool,           default=True,                                help='Whether to use LR scheduler or not')
+    parser.add_argument("--reduction_type",  type=str, default='mean', help='Reduction type used for MSE loss')
     # Model parameters ########################################################################################################################################################################
     parser.add_argument('--depth', type=int, default=12, help='' )
     parser.add_argument('--hidden_size', type=int, default=1024, help='Size of latent space')
@@ -86,7 +93,7 @@ def get_main_parser():
     # Data masking parameters ################################################################################################################################################################
     parser.add_argument('--neighborhood_type',              type=str,           default='nn_distance',              help='The method used to select the neighboring spots.', choices=['circular_hops', 'nn_distance'])
     parser.add_argument('--num_neighs',                     type=int,           default=18,                          help='Amount of neighbors to consider for context during imputation.')
-    parser.add_argument('--num_hops',                       type=int,           default=1,                          help='Amount of graph hops to consider for context during imputation if neighborhoods are built based on proximity rings.')
+    parser.add_argument('--num_hops',                       type=int,           default=-1,                          help='Amount of graph hops to consider for context during imputation if neighborhoods are built based on proximity rings.')
     # Visualization parameters ################################################################################################################################################################
     parser.add_argument('--gene_id',                        type=int,           default=0,                          help='Gene ID to plot.')
     # W&B usage parameters ####################################################################################################################################################################
@@ -235,6 +242,7 @@ def inference_function(dataloader, data, model, max_norm, min_norm, avg_tensor, 
     from model_stDiff.sample import sample_stDiff
     """
     Function designed to do inference for validation and test steps.
+    The data must be normalized between -1 to 1.
     Params:
         -dataloader (Pytorch.Dataloader): dataloader containing batches, each element has -> (st_data, st_masked_data, mask)
         -data (np.array): original st data
@@ -292,7 +300,7 @@ def inference_function(dataloader, data, model, max_norm, min_norm, avg_tensor, 
         prediction = prediction + avg_tensor.to(prediction.device)
         prediction = np.array(prediction)
 
-    # Arreglar mask_boolean
+    # La mascara aplica para todo
     mask_boolean = np.full(data.shape, True, dtype=bool)
     metrics_dict = get_metrics(data, prediction, mask_boolean)
     
@@ -490,3 +498,20 @@ def get_mask_extreme_completion(adata, mask):
     mask_extreme_completion[imp_values] = 1
     mask_extreme_completion[:,:,1:] = 0
     return mask_extreme_completion
+
+
+def compare_states(state1, state2, name="State"):
+    for key in state1.keys():
+        if isinstance(state1[key], torch.Tensor):
+            if not torch.equal(state1[key], state2[key]):
+                print(f"{name} mismatch: Key '{key}' differs.")
+                return False
+        elif isinstance(state1[key], dict):
+            # Recursively compare nested dictionaries
+            if not compare_states(state1[key], state2[key], name=f"{name}->{key}"):
+                return False
+        else:
+            if state1[key] != state2[key]:
+                print(f"{name} mismatch: Key '{key}' differs.")
+                return False
+    return True